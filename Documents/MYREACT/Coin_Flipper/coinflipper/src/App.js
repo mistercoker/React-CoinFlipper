3cfb8776ee695983c357aded32a9a57e509c0305
Edward Coker <edwardcoker27@gmail.com>
2020-02-17 14:03:05 +0000

    initial commit

---
+++ Documents/MYREACT/Coin_Flipper/coinflipper/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CoinContianer from './CoinContainer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CoinContianer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Documents/MYREACT/Coin_Flipper/coinflipper/src/CoinContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class CoinContainer extends Component {
+    static defaultProps = {
+        coins: [
+            {side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+            {side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
+        ]
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            curCoin: null,
+            nFlips: 0,
+            nHeads: 0,
+            nTails: 0
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    flipCoin() {
+
+    }
+    handleClick(e) {
+        this.flipCoin();
+    }
+    render() {
+        return (
+            <div>
+                <h2>Let's Flip A Coin</h2>
+                <button onClick={this.handleClick}>Flip A Coin</button>
+                <p>Out of {this.state.nFlips} flips, there has been {this.state.nHeads} heads and {this.state.nTails} tails</p>
+            </div>
+        )
+    }
+}
