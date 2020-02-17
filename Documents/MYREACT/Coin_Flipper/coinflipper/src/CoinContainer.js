8d353a14b9b82f631e5dd45a974078e2d36b98d5
Edward Coker <edwardcoker27@gmail.com>
2020-02-17 14:16:51 +0000

    added helper.js component

--- Documents/MYREACT/Coin_Flipper/coinflipper/src/CoinContainer.js
+++ Documents/MYREACT/Coin_Flipper/coinflipper/src/CoinContainer.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { choice } from './helpers';
+
 
 export default class CoinContainer extends Component {
     static defaultProps = {
